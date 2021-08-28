704d1a4bf2f8b287c863b15f3164e0b582a16ae6
Brian Smith <brian.bubbam@gmail.com>
2021-08-28 14:14:13 -0500

    commit tree a7ec803598cd7685539751c47ece40eff09c1835

--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,3 +1,40 @@
-export function fetchCats() {}
 
-export default function catsReducer() {}
+
+const initialState = {
+    entities: [], // array of cats
+    status: "idle", // loading state
+  };
+  
+
+export function fetchCats() {
+    return function (dispatch) {
+        dispatch({ type: "cats/catsLoading" });
+        fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+        .then((response) => {
+        return response.json();
+        })
+        .then((data) => {
+        // instead of logging here, call dispatch and send the cat JSON data to your store
+        dispatch({ type: "cats/catsLoaded", payload: data.images });
+        }); 
+    }
+}
+
+export default function catsReducer(state = initialState, action) {
+    switch (action.type) {
+        case "cats/catsLoading":
+          return {
+            ...state,
+            status: "loading",
+          };
+        case "cats/catsLoaded":
+          return {
+            ...state,
+            entities: action.payload,
+            status: "idle",
+          };
+        default:
+          return state;
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "bootstrap/dist/css/bootstrap.min.css";
+import catsReducer from "./features/cats/catsSlice.js";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+
+const store = createStore(catsReducer, composedEnhancer);
+
+ReactDOM.render(
+    <Provider store={store} >
+        <App />, document.getElementById("root")
+    </Provider>        
+    );
